abaf37a7b32f885d635362ebb388415febc81bb8
dzakwanfio <fiodzakwan@gmail.com>
2026-02-22 16:45:59 +0700

    "perubahan tipis"

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view ('auth/login', 'livewire.auth.login')->name('user.login');
-Route::view('/hse/dashboard', 'livewire.h-s-e.dashboard')->name('hse.dashboard');
-Route::view('/public/report/accident', 'livewire.public.report-accident')->name('public.report.accident');
+// Auth (Front-end only)
+Route::view('auth/login', 'livewire.auth.login')->name('user.login');
+
+// Dashboard (HSE Manager)
+Route::view('/hse-manager/dashboard', 'livewire.h-s-e-manager.dashboard')
+    ->name('hse.manager.dashboard');
+
+// Public (Pelapor tanpa login)
+Route::view('/public/report/accident', 'livewire.public.report-accident')
+    ->name('public.report.accident');
+
 Route::view('/public/report/unsafe', 'livewire.public.report-unsafe')
     ->name('public.report.unsafe');
